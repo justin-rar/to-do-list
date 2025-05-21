83b71d4665d21572c8d2d870d4737131dea786a9
justin-rar <justinrasyid09@gmail.com>
2025-05-21 14:41:20 +0700

    tambah void tipis tipis

--- TO-DO-LIST.cpp
+++ TO-DO-LIST.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+struct node {
+    string info;
+    node *kanan, *kiri;
+};
+
+node *awal, *akhir, *bantu, *hapus, *NB;
+
+void buatlistbaru();
+void sisipnode(char IB);
+void hapusnode(char IH);
+void bacamaju();
+void bacamundur();
+int listkosong();
+
 int main() {
     
     return 0;
